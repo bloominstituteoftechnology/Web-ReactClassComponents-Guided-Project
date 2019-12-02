636b413f43b99351ca95c283f15f64eb1fb7971d
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-02 11:49:33 +0100

    wip

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,50 @@
 import React, { useState } from 'react';
 
-export default class {
-  // implement
-  // it's OK if you don't have time to change the methods
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formValues: {
+        fname: '',
+        lname: '',
+      },
+    };
+  }
+
+  onValueChange = (event) => {
+    // what syntax to use????
+    // are we computing new state off of old state?
+    // we have access to name of the target
+    // AND the value
+    const { name, value } = event.target;
+
+    this.setState({
+      formValues: {
+        ...this.state.formValues,
+        [name]: value,
+      },
+    });
+  }
+
+  onFormSubmit = (event) => {
+
+  }
+
+  render() {
+    return (
+      <form className='component' onSubmit={this.onFormSubmit}>
+        <label>first name
+          <input value={this.state.formValues.fname} onChange={this.onValueChange} name='fname' />
+        </label><br />
+
+        <label>last name
+          <input value={this.state.formValues.lname} onChange={this.onValueChange} name='lname' />
+        </label><br />
+
+        <button>submit</button>
+      </form>
+    );
+  }
 }
 
 export function FormOld() {
